b6a65f929c866b2448bce73421f9828e279830e3
Nongdada <ronnachai_si@cmu.ac.th>
2023-02-07 17:17:05 +0700

    "คอมค้างครับ"

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -8,6 +8,7 @@ void shuffle(int &,int &,int &,int &);
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
 	
+
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
@@ -16,4 +17,16 @@ int main(){
 	}
 	
 	return 0;
+}
+
+#include<algorithm>
+void shuffle (int &a,int &b,int &c,int &d){
+	int o[]={a,b,c,d};
+	random_shuffle(o,o+4);
+	a=o[0];
+	b=o[1];
+	c=o[2];
+	d=o[3];
+
+	
 }
--- lab16_5.cpp
+++ lab16_5.cpp
@@ -1,21 +1,33 @@
-#include <iostream>
-#include <cstdlib>
-#include <ctime>
-using namespace std;
-
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
-
-int main(){
-	int a = 50, b = 100, c = 500, d = 1000;
-	
-	srand(time(0));	
-	
-	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
-	    cout << a << " " << b << " " << c << " " << d << "\n";
-	}
-	
-	return 0;
-}
-
-//Write definition of shuffle() using pointer here 
+#include <iostream> 
+ #include <cstdlib> 
+ #include <ctime> 
+ using namespace std; 
+  
+ void shuffle(int *,int *, int *, int *); //Modify input arguments to pointer  
+  
+ int main(){ 
+         int a = 50, b = 100, c = 500, d = 1000; 
+          
+         srand(time(0));         
+          
+         for(int i = 0;i < 10;i++){ 
+             shuffle(&a,&b,&c,&d); //Modify input arguments to pointer  
+             cout << a << " " << b << " " << c << " " << d << "\n"; 
+         } 
+          
+         return 0; 
+ } 
+  
+ //Write definition of shuffle() using pointer here  
+ void shuffle(int *a1,int *a2,int *a3,int *a4){ 
+         int *x[4] = {a1, a2, a3, a4}; 
+  
+         for(int i = 0; i < 4; i++){ 
+                 int rg = rand()%4; 
+                 int a = *x[i]; 
+                 *x[i]= *x[rg]; 
+                 *x[rg]= a; 
+         } 
+          
+         *a1 = *x[0]; *a2 = *x[1]; *a3 = *x[2]; *a4 = *x[3]; 
+ }
